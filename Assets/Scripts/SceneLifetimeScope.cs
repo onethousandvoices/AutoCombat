ab88f72ca2504d70b33854d2cb5d4f6ffb9f02f8
max <badasskid@me.com>
2026-02-20 21:58:43 +0700

    Plugins added

---
+++ Assets/Scripts/SceneLifetimeScope.cs
@@ -0,0 +1,9 @@
+using VContainer;
+using VContainer.Unity;
+
+public sealed class SceneLifetimeScope : LifetimeScope
+{
+    protected override void Configure(IContainerBuilder builder)
+    {
+    }
+}
